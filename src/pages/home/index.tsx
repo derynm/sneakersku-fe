d2ce4b00582b94b423a48fd5059a00083bf900db
derynm <dery02.nm@gmail.com>
2025-02-07 14:38:10 +0700

    initial project

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+function index() {
+  return (
+    <>
+        <div className="text-4xl font-bold text-center bg-red-600">
+            Home
+        </div>
+    </>
+  )
+}
+
+export default index
